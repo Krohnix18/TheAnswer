7d218dc8563678989160589a933a46c7691243ce
Gowtham Krishna <n10897402@qut.edu.au>
2022-10-29 20:30:32 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TheAnswer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            theQuestion();
+        }
+        
+        static void theQuestion ()
+        {
+            Console.WriteLine(42);
+        }
+    }
+}
